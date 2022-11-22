8e4e3c887c8151bd3a78aaed0945d1f19c4b6db5
TK <thiratikan@gmail.com>
2022-11-22 00:06:47 -0500

    update login

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import LoginForm from "./LoginForm";
 
 function Login() {
-  return <div>Login</div>;
+  const adminUser = {
+    email: "",
+    password: "",
+  };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    console.log(details);
+  };
+  const Logout = () => {
+    console.log("Logout");
+  };
+  return (
+    <div>
+      {user.email !== "" ? (
+        <div>
+          <h1>
+            Welcome, <span>{user.name}</span>
+          </h1>
+          <button>Next</button>
+          <button>Logout</button>
+        </div>
+      ) : (
+        <LoginForm />
+      )}
+    </div>
+  );
 }
 
 export default Login;
