ac303f39575eb68579bf697466f0f6dfec99fa68
TK <thiratikan@gmail.com>
2022-11-29 23:34:40 -0500

    Added toggle for Login and Register Tabs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Component/LoginPage";
 import Register from "./Component/Register";
+import { Tap } from "./styles/Style-Register";
 
 function App() {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
   return (
-    <>
-      <Login />;
-      <Register />
-    </>
+    <Tap>
+      <button
+        className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+        onClick={() => toggleTab(1)}
+      >
+        Login
+      </button>
+      <button
+        className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+        onClick={() => toggleTab(2)}
+      >
+        Register
+      </button>
+
+      <div
+        className={toggleState === 1 ? "content  active-content" : "content"}
+      >
+        <Login />
+      </div>
+      <div
+        className={toggleState === 2 ? "content  active-content" : "content"}
+      >
+        <Register />
+      </div>
+    </Tap>
   );
 }
 
