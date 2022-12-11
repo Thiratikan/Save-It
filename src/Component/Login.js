023b19fdb5c895ab124c918598b73ec28a8452b3
TK <thiratikan@gmail.com>
2022-12-11 17:02:28 -0500

    fix login page

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -3,7 +3,6 @@ import { Form, Button, Card, Alert, Container } from "react-bootstrap";
 import { useAuth } from "./contexts/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 import { Picture, Div } from "../styles/Style-Register";
-import { AuthProvider } from "./contexts/AuthContext";
 
 export default function Login() {
   const emailRef = useRef();
@@ -16,47 +15,54 @@ export default function Login() {
   async function handleSubmit(e) {
     e.preventDefault();
 
+    try {
+      setError("");
+      setLoading(true);
+      await login(emailRef.current.value, passwordRef.current.value);
+      history.push("/");
+    } catch {
+      setError("Failed to log in");
+    }
+
     setLoading(false);
   }
   return (
-    <AuthProvider>
-      <Container
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}
-      >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Card>
-            <Card.Body>
-              <Picture>
-                <img src="./images/SaveItLogoGreen.png" alt="" />
-              </Picture>
-              <h3 className="text-center mb-4">Log In</h3>
-              {error && <Alert variant="danger">{error}</Alert>}
-              <Form onSubmit={handleSubmit}>
-                <Form.Group id="email">
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" ref={emailRef} required />
-                </Form.Group>
-                <Form.Group id="password">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" ref={passwordRef} required />
-                </Form.Group>
-                <Div>
-                  <Button disabled={loading} className="w-100" type="submit">
-                    Log In
-                  </Button>
-                </Div>
-              </Form>
-              <div className="w-100 text-center mt-3">
-                <Link to="/forgot-password">Forgot Password?</Link>
-              </div>
-            </Card.Body>
-          </Card>
-          <div className="w-100 text-center mt-2">
-            Need an account? <Link to="/register">Sign Up</Link>
-          </div>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Card>
+          <Card.Body>
+            <Picture>
+              <img src="./images/SaveItLogoGreen.png" alt="" />
+            </Picture>
+            <h3 className="text-center mb-4">Log In</h3>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" ref={emailRef} required />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" ref={passwordRef} required />
+              </Form.Group>
+              <Div>
+                <Button disabled={loading} className="w-100" type="submit">
+                  Log In
+                </Button>
+              </Div>
+            </Form>
+            <div className="w-100 text-center mt-3">
+              <Link to="/forgot-password">Forgot Password?</Link>
+            </div>
+          </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+          Need an account? <Link to="/register">Sign Up</Link>
         </div>
-      </Container>
-    </AuthProvider>
+      </div>
+    </Container>
   );
 }
